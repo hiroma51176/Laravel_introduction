7b31acf2d34bf3d80550a147977cd2fce408f58b
hiroma <code51176@gmail.com>
2020-03-23 15:45:26 +0900

    Chapter2 finished

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,30 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function index($id='noname', $pass='unknown'){
-      return <<<E0F
 
-<html>
-<head>
-<title>Hello/index</title>
-<style>
-body {font-size: 16pt; color: #999;}
-h1 {font-size: 100pt; text-align: right; color: #eee;
-    margin: -40px 0px -50px 0px; }
-</style>
-</head>
-<body>
-  <h1>Index</h1>
-  <p>これは、Helloコントローラのindexアクションです。</p>
-  <ul>
-    <li>ID: {$id}</li>
-    <li>PASS: {$pass}</li>
-  </ul>
-</body>
-</html>
-E0F;
+    public function index(Request $request, Response $response) {
+      $html = <<<E0F
+      <html>
+      <head>
+      <title>Hello</title>
+      <style>
+      body {font-size:16pt; color:#999}
+      h1 {font-size:30pt; text-align: right; color: #eee;
+          margin: -15pt 0pt 0pt 0pt;}
+      </style>
+      </head>
+      <body>
+      <h1>Hello</h1>
+      <h3>Request</h3>
+      <pre>{$request}</pre>
+      <h3>Response</h3>
+      <pre>{$response}</pre>
+      </body>
+      </html>
+      E0F;
+      $response->setContent($html);
+      return $response;
     }
 }
