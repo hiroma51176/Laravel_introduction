546d3af22d98005ac4a26609bc3f855c9cafdaea
hiroma <code51176@gmail.com>
2020-03-24 16:50:59 +0900

    Chapter3-2 finished

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -8,27 +8,19 @@ use Illuminate\Http\Response;
 class HelloController extends Controller
 {
 
-    public function index(Request $request, Response $response) {
-      $html = <<<E0F
-      <html>
-      <head>
-      <title>Hello</title>
-      <style>
-      body {font-size:16pt; color:#999}
-      h1 {font-size:30pt; text-align: right; color: #eee;
-          margin: -15pt 0pt 0pt 0pt;}
-      </style>
-      </head>
-      <body>
-      <h1>Hello</h1>
-      <h3>Request</h3>
-      <pre>{$request}</pre>
-      <h3>Response</h3>
-      <pre>{$response}</pre>
-      </body>
-      </html>
-      E0F;
-      $response->setContent($html);
-      return $response;
+    public function index(Request $request) {
+      $data = [
+        'msg' => 'お名前を入力してください。'
+      ];
+      return view('hello.index', $data);
+    }
+
+    public function post(Request $request){
+      $msg = $request->msg;
+      $data = [
+        'msg' => 'こんにちは、' . $msg . 'さん！',
+      ];
+
+      return view('hello.index', $data);
     }
 }
