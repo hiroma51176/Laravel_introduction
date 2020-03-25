5df39e909c912cfa2a9e896532d7e31d460c9750
hiroma <code51176@gmail.com>
2020-03-25 15:55:45 +0900

    Chapter3-3 finished

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,18 +9,13 @@ class HelloController extends Controller
 {
 
     public function index(Request $request) {
-      $data = [
-        'msg' => 'お名前を入力してください。'
-      ];
-      return view('hello.index', $data);
+
+      $data = ['one', 'two', 'three', 'four', 'five'];
+      return view('hello.index', ['data' => $data]);
     }
 
     public function post(Request $request){
-      $msg = $request->msg;
-      $data = [
-        'msg' => 'こんにちは、' . $msg . 'さん！',
-      ];
 
-      return view('hello.index', $data);
+      return view('hello.index', ['msg' => $request->msg]);
     }
 }
